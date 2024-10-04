af3f9bed70e1afd1ccd88637b92dd55afa569d73
Juan Manuel Gauna <juanmanuelgauna@gmail.com>
2024-10-04 14:52:52 -0300

    se agrega vista carrusel

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { CartContext } from '../context/CartContext'; // Importa el contexto del carrito
+import { Carousel } from 'react-responsive-carousel'; // Importa el componente del carrusel
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Estilos del carrusel
 
 function ProductDetail() {
   const { id } = useParams();
@@ -28,7 +30,21 @@ function ProductDetail() {
   return (
     <div>
       <h1>{product.title}</h1>
-      <img src={product.pictures?.[0].url} alt={product.title} style={{ width: '300px' }} />
+
+      {/* Si el producto tiene más de una imagen, mostramos el carrusel */}
+      {product.pictures && product.pictures.length > 1 ? (
+        <Carousel>
+          {product.pictures.map((picture) => (
+            <div key={picture.id}>
+              <img src={picture.url} alt={product.title} style={{ width: '300px' }} />
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        // Si solo tiene una imagen, mostrarla sin el carrusel
+        <img src={product.pictures?.[0].url} alt={product.title} style={{ width: '300px' }} />
+      )}
+
       <p>Precio: ${product.price}</p>
       <p>{product.description}</p>
       <button onClick={() => addToCart(product)}>Agregar al carrito</button>
