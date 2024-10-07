cc7895323a1db2ba68747a53cf3a6368a689b98a
Juan Manuel Gauna <juanmanuelgauna@gmail.com>
2024-10-07 19:07:37 -0300

    cantidad productos en carrito y calculo

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 
 function Cart() {
-  const { cartItems, removeFromCart } = useContext(CartContext);
+  const { cartItems, removeFromCart, updateQuantity } = useContext(CartContext);
 
   return (
     <div>
@@ -14,6 +14,16 @@ function Cart() {
               <img src={item.thumbnail} alt={item.title} style={{ width: '100px' }} />
               <h3>{item.title}</h3>
               <p>Precio: ${item.price}</p>
+              <p>
+                Cantidad: 
+                <input
+                  type="number"
+                  value={item.quantity}
+                  min="1"
+                  onChange={(e) => updateQuantity(item.id, parseInt(e.target.value))}
+                />
+              </p>
+              <p>Total: ${(item.price * item.quantity).toFixed(2)}</p>
               <button onClick={() => removeFromCart(item.id)}>Eliminar</button>
             </li>
           ))}
