8b5db4e1465cd80e70baf061506a3ae7dbdde43c
Juan Manuel Gauna <juanmanuelgauna@gmail.com>
2024-10-14 22:33:01 -0300

    Se agrega stilo parecido a mercado libre

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Search from './components/Search';
 import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
-import Navbar from './components/Navbar'; // Importar el componente Navbar desde otro archivo
+import Navbar from './components/Navbar'; 
 
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Agrega la barra de navegación */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="/product/:id" element={<ProductDetail />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
 
 function Navbar() {
-  const { cartItems } = useContext(CartContext); // Usamos el contexto para obtener la cantidad de productos en el carrito
+  const { cartItems } = useContext(CartContext);
 
   return (
     <nav>
-      <h2>Mi Tienda</h2>
+      <h2>Mercado Libre Clone</h2>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li>
           <Link to="/cart">
-            🛒 Carrito ({cartItems.length}) {/* Muestra la cantidad de productos en el carrito */}
+            🛒 Carrito ({cartItems.length})
           </Link>
         </li>
       </ul>
